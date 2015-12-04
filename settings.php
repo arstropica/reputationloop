190f3111573e0465ba93d46e86f0af77ada51807
ArsTropica <aowilliams@arstropica.com>
2015-12-04 02:15:42 -0500

    Injected Model into Settings object

--- settings.php
+++ settings.php
@@ -2,13 +2,13 @@
 use Model\ReputationLoop;
 use Slim\Views\Twig;
 
-$twig = new Twig();
-$model = new ReputationLoop();
+$twig = new Twig ();
+$model = new ReputationLoop ();
 
-$settings = array(
-    'view' => $twig,
-    'templates.path' => '../View',
-    'model' => $model
+$settings = array (
+		'view' => $twig,
+		'templates.path' => '../View',
+		'model' => $model,
 );
 
 return $settings;
