a948bb3ec46484d1de5eee960807db4466efb89c
ArsTropica <aowilliams@arstropica.com>
2015-12-04 02:08:52 -0500

    Added index and get route actions

--- Controller/Main.php
+++ Controller/Main.php
@@ -1,26 +1,49 @@
 <?php
+
 namespace Controller;
 
 use ReputationLoop\Controller;
 
-class Main extends Controller
-{
-
-    public function index()
-    {
-        $this->render("index");
-    }
-
-    public function test($title)
-    {
-        $this->render("test", array(
-            "title" => $title,
-            "name" => "Test"
-        ));
-    }
-
-    public function error404()
-    {
-        $this->render('error', array(), 404);
-    }
+/**
+ * Main Controller
+ *
+ * @author nci
+ *        
+ */
+class Main extends Controller {
+	/**
+	 * Index Action
+	 *
+	 * @return void
+	 */
+	public function index() {
+		$title = 'Unknown Business';
+		
+		// Default $id
+		$id = 1;
+		
+		$model = $this->model;
+		$model->init ( $id );
+		
+		/* $business_info \Model\ReputationLoop\BusinessInfo */
+		$business_info = $model->getInfo ();
+		if ($business_info) {
+			$title = $business_info->getBusinessName ( $title );
+		}
+		
+		$this->render ( "index", 
+				array (
+						"title" => $title,
+						"description" => $title 
+				) );
+	}
+	
+	/**
+	 * Error Action
+	 *
+	 * @return void
+	 */
+	public function error404() {
+		$this->render ( 'error', array (), 404 );
+	}
 }
